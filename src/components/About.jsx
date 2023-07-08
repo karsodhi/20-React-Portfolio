9f1a0751eb7860791173fcdc9059e21ac0485e67
Kar Sodhi <karsodhi1212@gmail.com>
2023-07-07 20:04:54 -0500

    added component codes and updated About.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <p>
+                            About
+                        </p>
+                    </div>
+                    <div></div>
+                </div>
+                <div>
+                    <div>
+                        <p>Hi. I'm Karanvir Sodhi, nice to meet you. Please take a look around.</p>
+                    </div>
+                    <div>
+                        <p>I am passionate about building excellent software that improves
+                            the lives of those around me. I specialize in creating software
+                            for clients ranging from individuals and small-businesses all the
+                            way to large enterprise corporations.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
